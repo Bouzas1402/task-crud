4063953105094fa323cc337a6c17039e484b4b2a
Carlos Bouzas <carlosbouzas1991@gmail.com>
2026-04-15 10:24:41 +0200

    Update navigation in the layout

--- app/tasks/layout.tsx
+++ app/tasks/layout.tsx
@@ -1,21 +1,35 @@
 'use client';
-
+import { Suspense } from 'react';
 import { LinkButton } from '@/components/ui/LinkButton';
 import { useSelectedLayoutSegment } from 'next/navigation';
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <Suspense fallback={<div />}>
+      <LayoutContent>{children}</LayoutContent>
+    </Suspense>
+  );
+}
+
+function LayoutContent({ children }: { children: React.ReactNode }) {
   const segment = useSelectedLayoutSegment();
 
+  const isTasks = !segment;
   const isCreate = segment === 'create';
-
-  const href = isCreate ? '/tasks' : '/tasks/create';
-  const textButton = isCreate ? 'Lista de tareas' : 'Nueva tarea';
 
   return (
     <>
       <header className="fixed top-0 z-10 flex w-full items-center justify-between bg-transparent p-4">
         <div className="text-xl font-bold">Mis tareas</div>
-        <LinkButton href={href}>{textButton}</LinkButton>
+        <div className="flex gap-2">
+          <LinkButton hidden={isTasks} href="/tasks">
+            Lista de tareas
+          </LinkButton>
+
+          <LinkButton hidden={isCreate} href="/tasks/create">
+            Nueva tarea
+          </LinkButton>
+        </div>
       </header>
       <div className="pt-20">{children}</div>
     </>
